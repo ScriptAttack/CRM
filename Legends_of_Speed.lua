fbcbff43fd7f29b6af0642340e6e70632d5fa69f
ScriptAttack <118389987+ScriptAttack@users.noreply.github.com>
2022-11-19 00:02:25 +0530

    Update Legends_of_Speed.lua

--- Legends_of_Speed.lua
+++ Legends_of_Speed.lua
@@ -6,8 +6,6 @@ spawn(function()
         local args = {
     [1] = "collectOrb",
     [2] = "Red Orb",
-    [2] = "Yellow Orb",
-    [2] = "Gem",
     [3] = "City"
 }
 
@@ -23,6 +21,45 @@ spawn(function()
 }
 
 game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer(unpack(args))
+wait()
+end
+end)
+
+spawn(function()
+    while getgenv().orbEvent == true do
+        local args = {
+    [1] = "collectOrb",
+    [2] = "Gems",
+    [3] = "City"
+}
+
+game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+wait()
+end
+end)
+
+spawn(function()
+    while getgenv().orbEvent == true do
+        local args = {
+    [1] = "collectOrb",
+    [2] = "Yellow Orb",
+    [3] = "City"
+}
+
+game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+wait()
+end
+end)
+
+spawn(function()
+    while getgenv().orbEvent == true do
+        local args = {
+    [1] = "collectOrb",
+    [2] = "Orange Orb",
+    [3] = "City"
+}
+
+game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
 wait()
 end
 end)
